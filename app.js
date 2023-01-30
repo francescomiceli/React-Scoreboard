a7fa7bd33564a263023ab2bc06f447b1b518b3db
francesco.miceli <francescomiceli@outlook.com>
2023-01-30 19:49:52 +0100

    structure

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header(){
+const Header = () => {
     return (
       <header>
         <h1>Scoreboard</h1>
@@ -7,7 +7,42 @@ function Header(){
     );
 }
 
+const Player = () => {
+    return(
+        <div className="player">
+            <span className="player-name">
+                Francesco
+            </span>
+
+            <Counter />
+        </div>
+
+        
+    );
+}
+
+const Counter = () => {
+    return(
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">2</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return(
+        <div className="scoreboard">
+            <Header />
+
+            {/* Player list*/}
+            <Player />
+        </div>
+    );
+}
+
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
